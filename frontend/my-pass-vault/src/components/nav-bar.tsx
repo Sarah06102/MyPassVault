102afa722767486a0e9ae15812348f290dd80847
Sarah06102 <sarah.hra06@gmail.com>
2025-05-31 14:53:43 -0700

    added about, features, and tools sections

--- frontend/my-pass-vault/src/components/nav-bar.tsx
+++ frontend/my-pass-vault/src/components/nav-bar.tsx
@@ -8,11 +8,12 @@ const NavBar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50">
         <div className="justify-between py-4 px-8 space-x-6 text-white flex items-center bg-violet-700">
-            <Link to="/"className="font-bold text-lg">MyPassVault</Link>
+            <Link to="/"className="font-bold text-xl">MyPassVault</Link>
             <div className="flex items-center gap-3">
                 <Link to="/#About" className="cursor-pointer font-medium hover:bg-neutral-100/40 rounded-lg p-2 transition-all ease-in-out duration-300">About</Link>
                 <Link to="/#Tools" className="cursor-pointer font-medium hover:bg-neutral-100/40 rounded-lg p-2 transition-all ease-in-out duration-300">Tools</Link>
                 <Link to="/#Features" className="cursor-pointer font-medium hover:bg-neutral-100/40 rounded-lg p-2 transition-all ease-in-out duration-300">Features</Link>
+                <Link to="/#FAQs" className="cursor-pointer font-medium hover:bg-neutral-100/40 rounded-lg p-2 transition-all ease-in-out duration-300">FAQs</Link>
                 <div className="relative group">
                     <span className="items-center flex cursor-pointer font-medium hover:bg-neutral-100/40 rounded-lg p-2 transition-all ease-in-out duration-300 gap-1.5"><MdAccountCircle />Account</span>
                     <div className="p-5 group-hover:pointer-events-auto pointer-events-none left-1/2 -translate-x-1/2 absolute opacity-0 scale-95 group-hover:opacity-100 group-hover:scale-100 transform transition-all duration-300 flex flex-col bg-white text-black rounded shadow-lg min-w-max">
--- frontend/my-pass-vault/src/pages/home.tsx
+++ frontend/my-pass-vault/src/pages/home.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import NavBar from '../components/nav-bar'
+import { MdSettings } from "react-icons/md";
+import { MdAdminPanelSettings } from "react-icons/md";
+import { FaCircleCheck } from "react-icons/fa6";
+import { FaLockOpen } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 
 interface HomeProps {
@@ -45,56 +51,119 @@ const Home: React.FC<HomeProps> = ({ title }) => {
     <>
       {/* Nav bar */}
       <NavBar/>
-      <div className="gap-40 md:flex-row min-h-screen flex-col flex bg-violet-50 justify-center items-center p-8 rounded">
-      {/* Title */}
-      <div className="flex flex-col items-start mx-20">
-        <h1 className="text-5xl text-gray-800 font-extrabold mb-4">{ title }</h1>
-        <h3 className="text-xl text-gray-400">Password security, made simple.</h3>
-      </div>
-    
-      {/* Password Generator */}
-        <div className="bg-white rounded-2xl shadow-lg p-15 w-full max-w-lg flex flex-col gap-6">
-          {/* Checkbox options */}
-          <div className="grid grid-cols-2 gap-4">
-            <div className="flex items-center gap-2 whitespace-nowrap">
-              <input className="accent-violet-600" id="uppercase" checked={includeUppercase} onChange={(e) => setIncludeUppercase(e.target.checked)} type="checkbox"/>
-              <label htmlFor="uppercase" className="mb-1">Include Uppercase(A-Z)</label>
-            </div>
-            <div className="flex items-center gap-2 whitespace-nowrap">
-              <input className="accent-violet-600" id="lowercase" checked={includeLowercase} onChange={(e) => setIncludeLowercase(e.target.checked)} type="checkbox"/>
-              <label htmlFor="lowercase" className="mb-1">Include Lowercase(a-z)</label>
-            </div>
-            <div className="flex items-center gap-2 whitespace-nowrap">
-              <input className="accent-violet-600" id="numbers" checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)} type="checkbox"/>
-              <label htmlFor="numbers" className="mb-1">Include Numbers</label>
-            </div>
-            <div className="flex items-center gap-2 whitespace-nowrap">
-              <input className="accent-violet-600" id="symbols" checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.checked)} type="checkbox"/>
-              <label htmlFor="symbols" className="mb-1">Include Symbols</label>
+      <div className="bg-violet-50 overflow-x-hidden">
+        <div className="gap-40 md:flex-row min-h-screen flex-col flex justify-center items-center p-8 rounded">
+        {/* Title */}
+          <div className="flex flex-col items-start mx-20">
+            <h1 className="text-5xl text-gray-800 font-extrabold mb-4">{ title }</h1>
+            <h3 className="text-xl text-gray-400">Password security, made simple.</h3>
+            <div className="flex gap-4 mt-10 ml-10">
+              <FaLockOpen size={50} className="text-gray-600"/><FaArrowRightLong size={50}/><FaLock size={50} className="text-gray-600"/>
             </div>
           </div>
-
-          <div className="mt-5 mb-5 flex flex-col items-center justify-center gap-4">
-            <div className="flex items-center justify-center gap-3">
-              <label htmlFor="lengthInput" className="mb-1 font-medium">Password Length:</label>
-              {/* Editable length*/}
-              <input className="w-14 p-1 border border-gray-300 rounded text-center shadow-inner" id="lengthInput" type="number" min="4" max="36" value={length} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {const newLength = Number(e.target.value); if (newLength >= 4 && newLength <= 36) {
-                setLength(newLength);}}} />
-              {/* Slider */}
-              <input id="lengthSlider" className="accent-violet-600 w-64" type="range" min="4" max="36" value={length} onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setLength(Number(e.target.value))}/>
+      
+        {/* Password Generator */}
+          <div className="bg-white rounded-2xl shadow-lg p-18 flex flex-col gap-6">
+            {/* Checkbox options */}
+            <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+              <div className="flex items-center gap-2 whitespace-nowrap">
+                <input className="accent-violet-600" id="uppercase" checked={includeUppercase} onChange={(e) => setIncludeUppercase(e.target.checked)} type="checkbox"/>
+                <label htmlFor="uppercase" className="mb-1">Include Uppercase(A-Z)</label>
+              </div>
+              <div className="flex items-center gap-2 whitespace-nowrap">
+                <input className="accent-violet-600" id="lowercase" checked={includeLowercase} onChange={(e) => setIncludeLowercase(e.target.checked)} type="checkbox"/>
+                <label htmlFor="lowercase" className="mb-1">Include Lowercase(a-z)</label>
+              </div>
+              <div className="flex items-center gap-2 whitespace-nowrap">
+                <input className="accent-violet-600" id="numbers" checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)} type="checkbox"/>
+                <label htmlFor="numbers" className="mb-1">Include Numbers</label>
+              </div>
+              <div className="flex items-center gap-2 whitespace-nowrap">
+                <input className="accent-violet-600" id="symbols" checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.checked)} type="checkbox"/>
+                <label htmlFor="symbols" className="mb-1">Include Symbols</label>
+              </div>
             </div>
 
-            {/* Show Password */}
-            <div className="mt-6 flex justify-center text-center">
-              <label className="font-medium text-lg">Your Password:</label>
-              <span className="ml-2 shadow-inner border px-3 border-gray-300 rounded">{generatedPassword ? generatedPassword : null}</span>
-            </div>
-            <div className="flex gap-2 mt-10">
-              <button className="cursor-pointer hover:bg-violet-700 transition-colors duration-300 flex bg-violet-600 text-white py-2 px-3 rounded-3xl" onClick={handleCopyPassword}>Copy password</button>
-              <button className="cursor-pointer hover:bg-violet-200 transition-colors duration-300 flex bg-violet-100 text-violet-600 py-2 px-3 rounded-3xl" onClick={handleRefreshPassword}>Regenerate password</button>
+            <div className="mt-5 mb-5 flex flex-col items-center justify-center gap-4">
+              <div className="flex items-center justify-center gap-3">
+                <label htmlFor="lengthInput" className="mb-1 whitespace-nowrap font-medium">Password Length:</label>
+                {/* Editable length*/}
+                <input className="w-14 p-1 border border-gray-300 rounded text-center shadow-inner" id="lengthInput" type="number" min="4" max="36" value={length} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {const newLength = Number(e.target.value); if (newLength >= 4 && newLength <= 36) {
+                  setLength(newLength);}}} />
+                {/* Slider */}
+                <input id="lengthSlider" className="accent-violet-600 w-64" type="range" min="4" max="36" value={length} onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setLength(Number(e.target.value))}/>
+              </div>
+
+              {/* Show Password */}
+              <div className="mt-6 flex justify-center text-center">
+                <label className="font-medium text-lg">Your Password:</label>
+                <span className="ml-2 shadow-inner border px-3 border-gray-300 rounded">{generatedPassword ? generatedPassword : null}</span>
+              </div>
+              <div className="flex gap-2 mt-10">
+                <button className="cursor-pointer hover:bg-violet-700 transition-colors duration-300 flex bg-violet-600 text-white py-2 px-3 rounded-3xl" onClick={handleCopyPassword}>Copy password</button>
+                <button className="cursor-pointer hover:bg-violet-200 transition-colors duration-300 flex bg-violet-100 text-violet-600 py-2 px-3 rounded-3xl" onClick={handleRefreshPassword}>Regenerate password</button>
+              </div>
             </div>
           </div>
-        </div>  
+        </div>
+        {/* About Section */}
+        <section className="mt-30 text-center">
+          <h2 className="text-3xl font-bold mb-4">What is MyPassVault?</h2>
+          <p className="max-w-xl mx-auto text-gray-600">
+            MyPassVault is a secure and easy-to-use password generator that helps you create strong passwords to protect your online accounts.
+          </p>
+        </section>  
+        {/* Tools Section */}
+        <section className="mt-50 text-center bg-violet">
+          <h2 className="text-3xl font-extrabold text-gray-800 mb-10 text-center">Tools</h2>
+          <ul className="space-y-6 text-center flex flex-col items-center border border-gray-400 shadow-inner shadow-gray-300/80 bg-white rounded-2xl m-10 p-5 max-w-4xl mx-auto">
+            <li className="flex flex-col items-center gap-4">
+                  <h3 className="text-xl font-semibold text-gray-800 italic underline-animation flex justify-center items-center gap-3"><MdSettings size={40}/> Password Generator</h3>
+                  <p className="text-gray-600 ">Generate secure and random passwords effortlessly.</p>
+            </li>
+            <li className="flex flex-col items-center gap-4">
+                <h3 className="text-xl font-semibold text-gray-800 italic underline-animation flex justify-center items-center gap-3"><FaCircleCheck size={30}/>Password Checker</h3>
+                <p className="text-gray-600">Assess the strength of your passwords in real-time.</p>
+            </li>
+            <li className="flex flex-col items-center gap-4">
+                <h3 className="text-xl font-semibold text-gray-800 italic underline-animation flex justify-center items-center gap-3"><MdAdminPanelSettings size={40}/>Password Manager</h3>
+                <p className="text-gray-600">Store and manage your passwords securely and conveniently.</p>
+            </li>
+          </ul>
+        </section>
+        {/* Features Section */}
+        <section className="text-center p-30 mt-20">
+          <div className="max-w-6xl mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-10">Features</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
+                <h3 className="text-xl font-semibold mb-2">Robust Password Generator</h3>
+                <p className="text-gray-600">Create secure, random passwords to keep your accounts safe.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
+                <h3 className="text-xl font-semibold mb-2">Customizable Length</h3>
+                <p className="text-gray-600">Choose the perfect password length that fits your needs.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
+                <h3 className="text-xl font-semibold mb-2">Flexible Character Set</h3>
+                <p className="text-gray-600">Mix and match uppercase, lowercase, numbers, and symbols.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
+                <h3 className="text-xl font-semibold mb-2">Instant Copy</h3>
+                <p className="text-gray-600">Copy your password to the clipboard with a single click.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
+                <h3 className="text-xl font-semibold mb-2">Quick Refresh</h3>
+                <p className="text-gray-600">Generate a brand new password in seconds.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
+                <h3 className="text-xl font-semibold mb-2">Secure Storage</h3>
+                <p className="text-gray-600">Save your passwords to your MyPassVault account for future use.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* FAQ Section */}
       </div>
     </>
   );
