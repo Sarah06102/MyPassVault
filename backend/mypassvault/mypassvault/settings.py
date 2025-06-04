ad15bb427353fcb4588a452c238f2d8e4e1d7a40
Sarah06102 <sarah.hra06@gmail.com>
2025-06-04 14:28:07 -0700

    Updated settings.py variables

--- backend/mypassvault/mypassvault/settings.py
+++ backend/mypassvault/mypassvault/settings.py
@@ -200,7 +200,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mypassvaulthelp@gmail.com'
-EMAIL_HOST_PASSWORD = 'wbardgwdgmfkddad'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 ssl._create_default_https_context = ssl._create_unverified_context
