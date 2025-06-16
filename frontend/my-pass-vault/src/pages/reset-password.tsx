83c9d032b318cb63b5c5f0fb40d472bebe9ea3c7
Sarah06102 <sarah.hra06@gmail.com>
2025-06-15 17:59:27 -0700

    Fixed url in reset password

--- frontend/my-pass-vault/src/pages/reset-password.tsx
+++ frontend/my-pass-vault/src/pages/reset-password.tsx
@@ -14,7 +14,7 @@ const ResetPassword: React.FC = () => {
             const csrfToken = document.cookie.split('; ').find(row => row.startsWith('csrftoken='))?.split('=')[1] || '';
             const formData = new URLSearchParams();
             formData.append('email', enteredEmail);
-            const response = await fetch(`${apiUrl}/api/password_reset/`, {
+            const response = await fetch(`${apiUrl}/password_reset/`, {
               method: 'POST',
               headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'X-CSRFToken': csrfToken, },
               credentials: 'include',
